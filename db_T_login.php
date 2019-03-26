73149c1308be85da5dc047a1dbc38139e84000f3
dhruvds9 <dhruvsharmads9@gmail.com>
2019-03-26 09:59:23 +0530

    db files changed

--- db_T_login.php
+++ db_T_login.php
@@ -1,7 +1,14 @@
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body style="background-image: url('bg3.jpg'); background-repeat: round">
 <?php
 $host_link = mysqli_connect('localhost','root','','intra_vit');
-echo "Connected to localhost server and intra_VIT database successfully <br> ";
-
+?>
+<div align="center">
+  <h3>Connected to localhost server and intra_VIT database successfully</h3><br>
+</div>
+<?php
 $Tno = $_POST["T_No"];
 $pass = $_POST["Password"];
 $reg_login = mysqli_query($host_link,"SELECT * FROM t_data WHERE Passwod='$pass' AND T_no ='$Tno'");
@@ -9,16 +16,31 @@ if(mysqli_num_rows($reg_login)>0)
 {
     $reg_name = mysqli_fetch_assoc(mysqli_query($host_link,"SELECT * FROM t_data WHERE T_no ='$Tno'"));
     $name=$reg_name['T_name'];
-    echo "Logged in Successfully<br>";
-    echo "Hello:$name <br><br>";
-    echo '<form name="continue" action="form_display.php" method="post">';
-    echo '<input type="submit" value="Continue">';
-    echo '</form>';
+?>
+    <div align="center">
+      <h4>Logged in Successfully</h4><br>
+    </div>
+    <div align="center">
+      <h4><?php echo "Hello:$name" ?></h4><br>
+    </div>;
+    <form class="col-md-6 auto" name="continue" style="margin-left:25%; margin-right:25%; margin-top:10%" action="form_display.php" method="post">
+     <div align="center">
+       <button type="submit" class="btn btn-primary">Continue</button>
+     </div>
+    </form>
+<?php
     $file = fopen("T_login.txt", 'w');
-fwrite($file, $Tno);
+    fwrite($file, $Tno);
 }
 else
 {
-    echo "Login error: Please make sure you are registered with the entered username and password";
+?>
+    <form class="col-md-6 auto" style="margin-left:25%; margin-right:25%; margin-top:10%">
+      <div align="center">
+        <h3>Login error: Please make sure you are registered with the entered username and password</h3><br>
+      </div>
+    </form>
+<?php
 }
 ?>
+</body>
--- db_register.php
+++ db_register.php
@@ -1,14 +1,28 @@
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body style="background-image: url('bg3.jpg'); background-repeat: round">
 <?php
 $host_link = mysqli_connect('localhost','root','','intra_vit');
-echo "Connected to localhost server and intra_VIT database successfully <br> ";
+?>
+<div align="center">
+  <h3>Connected to localhost server and intra_VIT database successfully</h3><br>
+</div>
+<?php
 $name = $_POST["Name"];
 $regno = $_POST["Reg_No"];
 $pass = $_POST["Password"];
 $reg = mysqli_query($host_link,"SELECT * FROM stud_reg WHERE Reg_No ='$regno'");
 if(mysqli_num_rows($reg)>0)
 {
-   echo "Registration failed<br>";
-   echo "$regno is already registered";
+?>
+   <div align="center">
+     <h4>Registration failed</h4><br>
+   </div>
+   <div align="center">
+     <h4><?php echo $regno ?> is already registered</h4><br>
+   </div>;
+<?php
 }
 else
 {
@@ -16,11 +30,20 @@ else
     $sql2="INSERT INTO stud_login (Reg_No,Passwrd) VALUES('$regno','$pass')";
     if((mysqli_query($host_link,$sql1)) and (mysqli_query($host_link,$sql2)))
     {
-        echo " Registration of $regno successful";
+?>
+        <div align="center">
+          <h4>Registration of <?php echo $regno ?> successful</h4><br>
+        </div>;
+<?php
     }
     else
     {
-        echo "Error in Processing";
+?>
+        <div align="center">
+          <h4>Error in Processing</h4><br>
+        </div>
+<?php
     }
 }
 ?>
+</body>
